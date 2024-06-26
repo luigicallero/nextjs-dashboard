a63ef2257c441d63b09206e03b15ba62f85d55dd
luigicallero <luigicallero@gmail.com>
2024-06-26 19:08:12 +1200

    Partial PreRendering ppr disabled

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        ppr: 'incremental',
-    },
+    // experimental: {
+    //     ppr: 'incremental',
+    // },
 };
 
 module.exports = nextConfig;
